8399b475b14dbf155caaee8bb7877c52a6d3d85f
Piyush7034 <piyushshukla2100@gmail.com>
2025-02-13 13:10:15 +0530

    Certify helper repo bootstrapping
    Signed-off-by: Piyush7034 <piyushshukla2100@gmail.com>

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+W
+'src/main/java/io/mosip/certify/App.java,e\8\e89c66d462487fe2739d6b5d9416eecab4bf9249
+s
+Csrc/main/java/io/mosip/certify/CertifyHelperServiceApplication.java,6\7\674608b38e472f1c1bbdccc020f65d5296fa1f77
---
+++ src/main/java/io/mosip/certify/CertifyHelperServiceApplication.java
@@ -0,0 +1,13 @@
+package io.mosip.certify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertifyHelperServiceApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(CertifyHelperServiceApplication.class, args);
+    }
+}
